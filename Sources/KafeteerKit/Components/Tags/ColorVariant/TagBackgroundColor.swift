f556d4f0ddc391fbaa0a9bbb75ba32ad5d10b519
Oscar De Moya <oscar.demoya@koombea.com>
2024-08-05 00:24:44 -0500

    Update color palette

--- Sources/KafeteerKit/Components/Tags/ColorVariant/TagBackgroundColor.swift
+++ Sources/KafeteerKit/Components/Tags/ColorVariant/TagBackgroundColor.swift
@@ -13,73 +13,73 @@ public extension TagColor {
         case .blue:
             switch variant {
             case .light: Color(.blue100)
-            case .base: Color(.blue500)
+            case .base: Color(.blue400)
             case .dark: Color(.blue800)
             }
         case .indigo:
             switch variant {
             case .light: Color(.indigo100)
-            case .base: Color(.indigo500)
+            case .base: Color(.indigo400)
             case .dark: Color(.indigo800)
             }
         case .purple:
             switch variant {
             case .light: Color(.purple100)
-            case .base: Color(.purple500)
+            case .base: Color(.purple400)
             case .dark: Color(.purple800)
             }
         case .pink:
             switch variant {
             case .light: Color(.pink100)
-            case .base: Color(.pink500)
+            case .base: Color(.pink400)
             case .dark: Color(.pink800)
             }
         case .red:
             switch variant {
             case .light: Color(.red100)
-            case .base: Color(.red500)
+            case .base: Color(.red400)
             case .dark: Color(.red800)
             }
         case .orange:
             switch variant {
             case .light: Color(.orange100)
-            case .base: Color(.orange500)
+            case .base: Color(.orange400)
             case .dark: Color(.orange800)
             }
         case .brown:
             switch variant {
             case .light: Color(.brown100)
-            case .base: Color(.brown500)
+            case .base: Color(.brown400)
             case .dark: Color(.brown800)
             }
         case .yellow:
             switch variant {
             case .light: Color(.yellow100)
-            case .base: Color(.yellow500)
+            case .base: Color(.yellow400)
             case .dark: Color(.yellow800)
             }
         case .lime:
             switch variant {
             case .light: Color(.lime100)
-            case .base: Color(.lime500)
+            case .base: Color(.lime400)
             case .dark: Color(.lime800)
             }
         case .green:
             switch variant {
             case .light: Color(.green100)
-            case .base: Color(.green500)
+            case .base: Color(.green400)
             case .dark: Color(.green800)
             }
         case .teal:
             switch variant {
             case .light: Color(.teal100)
-            case .base: Color(.teal500)
+            case .base: Color(.teal400)
             case .dark: Color(.teal800)
             }
         case .mint:
             switch variant {
             case .light: Color(.mint100)
-            case .base: Color(.mint500)
+            case .base: Color(.mint400)
             case .dark: Color(.mint800)
             }
         }
