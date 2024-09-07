d8e3aa3725150ab6780804879470095c0e9cfb94
Oscar De Moya <oscar.demoya@koombea.com>
2024-09-07 15:57:45 -0500

    Add dash underline modifier

---
+++ Sources/KafeteerKit/Modifiers/DashedUnderlineModifier.swift
@@ -0,0 +1,43 @@
+//
+//  DashedUnderlineModifier.swift
+//
+//
+//  Created by Oscar De Moya on 6/07/24.
+//
+
+import SwiftUI
+import KafeteerKit
+
+public extension View {
+    func dashedUnderline(borderWidth: BorderWidth = .nano,
+                         borderColor: Color = .borderColor,
+                         dash: [CGFloat] = [Spacing.quark.value]) -> some View {
+        modifier(
+            DashedUnderlineModifier(
+                borderWidth: borderWidth,
+                borderColor: borderColor,
+                dash: dash
+            )
+        )
+    }
+}
+
+private struct DashedUnderlineModifier: ViewModifier {
+    var borderWidth: BorderWidth
+    var borderColor: Color
+    var dash: [CGFloat]
+    
+    func body(content: Content) -> some View {
+        content
+            .overlay(
+                GeometryReader { geometry in
+                    Path { path in
+                        path.move(to: CGPoint(x: 0, y: geometry.size.height))
+                        path.addLine(to: CGPoint(x: geometry.size.width, y: geometry.size.height))
+                    }
+                    .stroke(style: StrokeStyle(lineWidth: borderWidth.rawValue, dash: dash))
+                    .foregroundColor(borderColor)
+                }
+            )
+    }
+}
